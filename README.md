8bffe3d0fb03c5e24a5f046d7b673778cb156c81
connect0S <46776254+0merdg@users.noreply.github.com>
2023-02-22 19:33:56 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+<h1 align="center">Heroku'da Webhook İle Çalışan Deneysel Bir Bot</h1>
+<p align="center">
+  <a href="https://github.com/Turkce-Botlar-Sohbet/Telegram_Bot">
+    <img src="https://socialify.git.ci/Turkce-Botlar-Sohbet/Telegram-Bot/image?description=1&font=Bitter&forks=1&issues=1&logo=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F71149025%3Fs%3D200%26v%3D4&pattern=Circuit%20Board&pulls=1&stargazers=1&theme=Dark" alt="Cover Image" width="500">
+  </a>
+  <p align="center">
+
+<hr>
+
+<h3 align="center">Nasıl Kurulur?</h3>
+
+**Zor Kurulum** <sub>(Tavsiye edilen)</sup>
+
+_Bu depoyu kendi hesabınıza "Fork" edin ve [.env](./.env) dosyasını açıp [@BotFather](https://t.me/botfather)'dan aldığınız tokeni [BOT_API](./.env) yerine yapıştırın ve Heroku'dan aldığınız bağlantıyı [URL](./.env) yerine yapıştırın daha sonra Heroku sunucunuza deploy edin detaylı anlatım için: [Tıklayın](https://telegra.ph/KENDİ-TELEGRAM-BOTUNUZU-YAPIN-09-11)._
+
+**Kolay Kurulum**
+
+_Butona basın kutuları doldurun ve heroku sunucunuza dağıtın._
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Komutlar
+Komut                   | Açıklama
+----------------------- | ----------------------------------------    
+`/start`                | Bota start verdiğinizde atılan ilk mesaj.
+`/botsohbet`            | Size bir video gönderir.
+`selam`                 | Fotoğraf gönderir.     
+`/komut`                | Telegram adınızı gönderir.
+`/buton`                | Butonlu mesaj gönderir.
+`/who`                  | Kullanıcı bilgilerini gönderir.
+
+Detaylı Bilgi İçin: [Buraya Tıkla](https://telegra.ph/KENDİ-TELEGRAM-BOTUNUZU-YAPIN-09-11)
+
+Botun Yazımında Kullanılan Kütüphane: [telegraf.js](https://telegraf.js.org)
+
+### Telif Hakkı ve Lisans
+- Copyright &copy; 2021 &mdash; [Türkçe Botlar Sohbet](https://t.me/botsohbet)
+- [MIT License](./LICENSE)
---
+++ index.js
@@ -0,0 +1,133 @@
+require("dotenv").config();
+
+const BOT_API       = process.env.BOT_API || '';
+const PORT          = process.env.PORT || 3000;
+const URL           = process.env.URL || 'https://your-heroku-app.herokuapp.com';
+
+const { Telegraf, Markup } = require('telegraf')
+const bot       = new Telegraf(BOT_API);
+
+const config = require('./config');
+
+// Bota start verdiğinizde atılan ilk mesaj
+bot.start((ctx) => {
+    return ctx.reply("Hey");
+});
+
+
+bot.hears(/selam/ig, async (ctx, next) => {
+    await ctx.telegram.sendPhoto(ctx.chat.id,
+        'https://www.ajanskirim.com/wp-content/uploads/2019/03/1525995635Merhaba.jpg',
+        { caption:  `<b>${ctx.from.first_name}</b>`,  parse_mode: 'HTML' })
+    return next();
+});
+
+
+bot.command('botsohbet', async (ctx, next) => {
+    
+    await bot.telegram.sendDocument(ctx.chat.id, {
+        source: './dosyalar/botsohbet.mp4'
+    }, {
+        filename: 'botsohbet.mp4',
+        caption: 'https://t.me/botsohbet'
+    })
+    return next()
+    
+})
+
+
+bot.command('komut', async (ctx, next) => {
+    await ctx.telegram.sendMessage(ctx.chat.id, `<b>${ctx.from.first_name}</b>`, { parse_mode: 'HTML' })
+    return next();
+});
+
+
+async function searchMessage(ctx){
+    await ctx.reply('<b>Hangi arama motorunu kullanmak istiyorsunuz?</b>', {
+        parse_mode: 'HTML',
+        ...Markup.inlineKeyboard([
+            [Markup.button.url('Google', 'www.google.com')],
+            [ Markup.button.callback('Yok ben almıyım.', 'kapat'), Markup.button.callback('Diğer', 'all')]
+        ])
+    })
+}
+
+
+bot.action('all', async (ctx) => {
+    await ctx.answerCbQuery()
+    await ctx.editMessageText('Yandex, DuckDuckGo, Yahoo ?', Markup.inlineKeyboard([
+        [Markup.button.url('Yandex', 'https://yandex.com.tr/'), Markup.button.url('DuckDuckGo', 'https://duckduckgo.com/')],
+        [Markup.button.url('Yahoo', 'https://www.yahoo.com/')],
+        [Markup.button.callback('Geri', 'geri')]
+    ]))
+})
+
+
+bot.action('geri', ctx => {
+    ctx.deleteMessage()
+    searchMessage(ctx)
+})
+
+
+bot.action('kapat', ctx => {
+    ctx.answerCbQuery()
+    ctx.deleteMessage()
+});
+
+
+bot.command("buton", ctx => {
+    ctx.deleteMessage()
+    searchMessage(ctx)
+})
+
+
+function getUserLink(user) {
+    const lastName = user.last_name ? ` ${user.last_name}` : '';
+    const username = user.username ? ` \nKullanıcı Adı: @${user.username}` : '';
+    const userBio = user.bio ? ` \nBio: ${user.bio}` : '';
+    return `<a href="tg://user?id=${user.id}">${user.first_name}${lastName}</a>${username}${userBio}\nID: <code>${user.id}</code>`;
+}
+
+
+bot.command("who", async (ctx) => {
+    const Id = ctx.message.reply_to_message ? ctx.message.reply_to_message.from.id : ctx.message.from.id;
+    const messageId = ctx.message.reply_to_message ? ctx.message.reply_to_message.message_id : null;
+    const getUserInfo = await ctx.telegram.getChat(Id);
+    const getUser = [getUserInfo].map(getUserLink).join(', ')
+    return ctx.replyWithHTML(getUser,  { reply_to_message_id: messageId })
+});
+
+
+bot.use(
+    require('./handlers/middlewares'),
+    require('./plugin')
+);
+
+// Kodlarda hata çıkarsa bunun sayesinde çalışmaya devam eder.
+bot.catch((err) => {
+    console.log('Error: ', err)
+})
+
+// Botun kullanıcı adını alan bir kod.
+bot.telegram.getMe().then(botInfo => {
+    bot.options.username = botInfo.username
+    console.log(`Bot Başlatıldı! => ${bot.options.username}`)
+})
+
+// Heroku sitesinde botunuzun kullanıcı adı gözükür -> deneyselbot.herokuapp.com
+const cb = function(req, res) {
+    res.end(`${bot.options.username}`)
+}
+
+// Botun webhook ile çalışmasını sağlar.
+bot.launch({
+    webhook: {
+        domain: `${URL}`,
+        port: `${PORT}`,
+        cb
+    }
+})
+
+// Bu botumuzu nazikçe durdurmayı etkinleştirir.
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
